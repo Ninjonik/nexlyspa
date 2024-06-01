34ef1df2ca9e9c37e3ce2bbcc1cec087b60f0aa8
Peter <petricekzatko@gmail.com>
2024-06-01 22:23:54 +0200

    - added the CheckRoom cloud function

---
+++ functions/CheckRoom/src/main.js
@@ -0,0 +1,36 @@
+import { Client, Databases } from "node-appwrite";
+
+export default async ({ req, res }) => {
+  const client = new Client()
+    .setEndpoint(process.env.APPWRITE_ENDPOINT)
+    .setProject(process.env.APPWRITE_PROJECT)
+    .setKey(process.env.APPWRITE_KEY);
+
+  const database = new Databases(client);
+
+  if (req.method === "GET") {
+    const roomId = req.body.roomId;
+
+    try {
+      await database.getDocument("nexly", "rooms", roomId);
+      console.log(roomId, "exists");
+      return res.json({
+        success: true,
+        message: "Room with the specified room code does exist.",
+        status: true,
+      });
+    } catch (err) {
+      console.log(roomId, "doesn't exist");
+      return res.json({
+        success: true,
+        message: "Room with the specified room code does not exist.",
+        status: true,
+      });
+    }
+  }
+
+  return res.json({
+    success: false,
+    message: "Invalid Method",
+  });
+};
