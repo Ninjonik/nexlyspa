3bfda99f4890f0b77d55d6b7be61b936bdd79a24
Peter <petricekzatko@gmail.com>
2024-08-18 14:28:21 +0200

    - fixed env vars

--- server/index.js
+++ server/index.js
@@ -10,6 +10,14 @@ import leaveRoom from "./routes/leaveRoom.js";
 import sendMessage from "./routes/sendMessage.js";
 import startCall from "./routes/startCall.js";
 import "dotenv/config";
+import * as dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: `${__dirname}/.env` });
 
 import fs from "fs";
 import http from "http";
